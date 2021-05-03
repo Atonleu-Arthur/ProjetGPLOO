6ebb3a9d15b8603eb22a5bd0916bfa35ca0b0f7c
Manurd <77494933+Manurd@users.noreply.github.com>
2021-05-03 18:20:41 +0200

    test unitaire 2

---
+++ src/Test/Java/ComandeTest.java
@@ -0,0 +1,115 @@
+//package Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Model.Exceptions.NoAlbumFoundException;
+import Model.Exceptions.NoElementFoundException;
+import Model.Exceptions.NoPlayListFoundException;
+import Model.PlayList;
+import client.Controller.SimpleClient;
+import org.junit.jupiter.api.Test;
+
+import server.Controller.MusicHubController;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * test les different commande possible
+ *
+ * @author Emanuel Dabadie
+ */
+
+public class ComandeTest {
+
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private Socket socket;
+    //SimpleClient c2 = new SimpleClient();
+    String albumTitle = "Best of Beatles";
+    String playlist= "my favourite songs";
+    String elementTitle="Here Comes the Sun";
+    MusicHubController musicHubController = new MusicHubController();
+    Iterator<PlayList> itpl = musicHubController.playlists();
+    PlayList pl= itpl.next();
+
+
+
+
+
+    @Test
+    void testgetAlbumsTitlesSortedByDate() throws IOException, ClassNotFoundException {
+       // output.writeObject("t");
+       musicHubController.getAlbumsTitlesSortedByDate();
+       // assertEquals((String) input.readObject(), musicHubController.getAlbumsTitlesSortedByDate());
+       // assertEquals((List<String>) input.readObject(), musicHubController.availableCommands());
+    }
+
+    @Test
+    void testgetAlbumSongsSortedByGenre() throws IOException, ClassNotFoundException, NoAlbumFoundException {
+      // musicHubController.getAlbumSongsSortedByGenre(albumTitle);
+        // output.writeObject("g");
+       // assertEquals((String) input.readObject(), musicHubController.getAlbumSongsSortedByGenre(albumTitle));
+      //  assertEquals((List<String>) input.readObject(), musicHubController.availableCommands());
+    }
+
+    @Test
+    void testgetAlbumSongs() throws IOException, ClassNotFoundException, NoAlbumFoundException {
+      //  musicHubController.getAlbumSongs(albumTitle);
+        //output.writeObject("d");
+       // assertEquals((String) input.readObject(), musicHubController.getAlbumSongs(albumTitle));
+      //  assertEquals((List<String>) input.readObject(), musicHubController.availableCommands());
+    }
+
+    /**
+     * On test si les different élément on bien été sauvegarder
+     */
+    @Test
+    void testsave(){
+        musicHubController.saveElements();
+        musicHubController.saveAlbums();
+       musicHubController.savePlayLists();
+    }
+
+    /**
+     * On test si on peut bien suprimer un playlist
+     *
+     * @throws NoPlayListFoundException si jamais on trouve pas la playlist en question
+     */
+    @Test
+    void testdeletePlayList() throws NoPlayListFoundException {
+        musicHubController.deletePlayList(playlist);
+    }
+
+    /**
+     * On test si on peux bien ajouter une playlist
+     * Et si on peux ajouter une music a une playlist
+     *
+     * @throws NoElementFoundException si on trouve pas la music en question qu'on veux rajouter
+     * @throws NoPlayListFoundException Si on trouve pas la playliste
+     */
+    @Test
+    void testaddPlayList() throws NoElementFoundException, NoPlayListFoundException {
+       musicHubController.addPlaylist(pl);
+       musicHubController.addElementToPlayList(elementTitle, playlist);
+   }
+   /*@Test
+    void testaddElementToAlbum(){
+        try {
+            musicHubController.addElementToAlbum(elementTitle, albumTitle);
+        } catch (NoAlbumFoundException e) {
+            e.printStackTrace();
+        } catch (NoElementFoundException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+
+}
